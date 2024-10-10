c1d57f17333ee91583f518eb369becfc475552aa
Abdulwahab Sani <33281871+aabsani@users.noreply.github.com>
2024-10-10 19:51:45 +0100

    Update App.jsx
    Re-implemented access of json file. Previous version used mock API with json server. Updated version access data.json directly

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@
 import { useEffect, useState } from "react";
 import Comment from "./components/Comment";
 import Reply from "./components/Reply";
+import data from "../data/data.json";
 
 function App() {
   const [comments, setComments] = useState([]);
-  const BASE_URL = "http://localhost:8000";
 
   function handleAddComment(comment) {
     setComments((newReply) => [...newReply, comment]);
@@ -28,66 +28,6 @@ function App() {
 
     setComments(updatedData);
   };
-
-  // ****** same as above but different implementation ******* //
-
-  // function handleEditComment(itemId, newContent, isComment) {
-  //   setComments((comments) =>
-  //     comments.map((comment) => {
-  //       if (isComment && comment.id === itemId) {
-  //         return { ...comment, content: newContent };
-  //       }
-  //       if (!isComment) {
-  //         return {
-  //           ...comment,
-  //           replies: comment.replies.map((reply) =>
-  //             reply.id === itemId ? { ...reply, content: newContent } : reply
-  //           ),
-  //         };
-  //       }
-  //       return comment;
-  //     })
-  //   );
-  // }
-
-  // function handleAddReply(newReply, parentCommentId, parentReplyId) {
-  //   setComments((prevComments) => {
-  //     const updateComments = [...prevComments];
-
-  //     // Recursive function to find the parent comment or parent reply
-  //     const findParentItem = (items, targetId) => {
-  //       for (const item of items) {
-  //         if (item.id === targetId) {
-  //           return item;
-  //         }
-  //         if (item.replies) {
-  //           const parentItem = findParentItem(item.replies, targetId);
-  //           if (parentItem) {
-  //             return parentItem;
-  //           }
-  //         }
-  //       }
-  //       return null;
-  //     };
-
-  //     // Find the parent item (either comment or reply)
-  //     const parentItem = findParentItem(
-  //       updateComments,
-  //       parentReplyId || parentCommentId
-  //     );
-
-  //     // Add the new reply to the parent item's replies array
-  //     if (parentItem) {
-  //       parentItem.replies = parentItem.replies || [];
-  //       parentItem.replies.push(newReply);
-  //     } else {
-  //       // If parent item is not found, add it to the top level comments
-  //       updateComments.push(newReply);
-  //     }
-
-  //     return updateComments;
-  //   });
-  // }
 
   function handleAddReply(newReply, parentCommentId, parentReplyId) {
     setComments((prevComments) => {
@@ -147,17 +87,7 @@ function App() {
   }
 
   useEffect(function () {
-    async function fetchComments() {
-      try {
-        const res = await fetch(`${BASE_URL}/comments`);
-        const data = await res.json();
-        setComments(data);
-      } catch (error) {
-        alert("There was an error fetching the data");
-        console.error(error);
-      }
-    }
-    fetchComments();
+    setComments(data.comments);
   }, []);
 
   return (
